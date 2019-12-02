ed7669a0b990b4c22619a580867d9f5ceb301855
david colvin <davidmcolvin@gmail.com>
2019-12-01 21:16:48 -0500

    Initial commit demonstrating the open closed principle

---
+++ OCPLibrary/Accounts/ExecutiveAccounts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCPLibrary
+{
+  public class ExecutiveAccounts : IAccounts
+  {
+    EmployeeModel output = new EmployeeModel();
+
+
+public EmployeeModel Create(IApplicantModel person)
+    {
+      output.FirstName = person.FirstName;
+      output.LastName = person.LastName;
+      output.EmailAddress = $"{person.FirstName}{person.LastName}@acmecorp.com";
+
+      output.IsManager = true;
+      output.IsExecutive = true;
+
+      return output;
+    }
+  }
+}
---
+++ OCPLibrary/Applicants/TechnicianModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCPLibrary
+{
+  public class TechnicianModel
+  {
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public IAccounts AccountProcessor { get; set; } = new Accounts();
+  }
+}
